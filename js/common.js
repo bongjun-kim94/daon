a64c41da5369f8c13a8a4fe47fe72f240f885462
bongjun-kim94 <74786253+bongjun-kim94@users.noreply.github.com>
2021-03-19 17:24:50 +0900

    daon

--- js/common.js
+++ js/common.js
@@ -11,19 +11,22 @@ $(function(){
         $('.nav_menu a').eq(localStorage.pageNum).css({ "font-weight":"bold" });
     }
 
-    $(".regular").slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay:true,
-        autoplaySpeend:1500,
-        arrows:true,
-        fade:true
-        // speend:1000
-        // centerMode: true,
-        // vertical: true
-      });
+    // ul태그에 slide 클래스가 있을 때만 실행
+    if($('ul').hasClass('slider')){
+      $(".regular").slick({
+          dots: true,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay:true,
+          autoplaySpeend:1500,
+          arrows:true,
+          fade:true
+          // speend:1000
+          // centerMode: true,
+          // vertical: true
+        });
+    }
   
       /* $('.regular').on({
         'beforeChange':function(event, slick, currentSlide, nextSlide){
@@ -33,21 +36,52 @@ $(function(){
           //alert('after');
         }
       }); */
+  
+
+  // select option 페이지 이동
+  var seletNum = 0;
+  $('select option').each(function(i){
+    if($(this).attr('value') == localStorage.select){
+      seletNum = i;
+    }
+  });
+
+  $('select option').eq(seletNum).prop('selected',true);
 
 
+  // 모바일 상태 top버튼
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 700) {
+        $('.goTop').fadeIn();
+    } else {
+        $('.goTop').fadeOut();
+    }
+  });
+
+  $(".goTop").click(function() {
+      $('html, body').animate({
+          scrollTop : 0
+      }, 500);
       
-
-      
-
+  });
 });
 
+// 방 정보 indi
 function changeSelect(url){
   var selectB = document.querySelector('#changes optgroup option');
   //window.location.href=this.value
+  localStorage.select = url;
   location.href = url;
   //var selectValue = selectB.options[selectB.selectedIndex].text;
   //select element에서 선택된 option의 text가 저장된다.
   // console.log(selectText);
 }
-        
-    
+
+// 모바일 버거메뉴 open, off
+function openNav() {
+  document.getElementById("mySidenav").style.width = "50%";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+} 
--- js/main.js
+++ js/main.js
@@ -25,7 +25,4 @@ $(function(){
         }
         $('.imgList img').eq(imgNum).fadeIn();
     }
-
-
 });
-
